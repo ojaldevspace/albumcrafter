1e732fd20cb22934ac891b056f468bd252070d96
Ojal Kumar <ojalkumar76@gmail.com>
2025-04-20 19:44:21 +0530

    handle view button for the view-job page

---
+++ src/types/ViewFormData.ts
@@ -0,0 +1,14 @@
+// types/ViewFormData.ts
+
+export interface ViewFormData {
+    jobNumber: string;
+    jobName: string;
+    jobType: string;
+    photographer: string;
+    location: string;
+    dealerName: string;
+    dealerMobileNumber: string;
+    eventDate: Date | null;
+    flipbookUrl: string;
+  }
+  
